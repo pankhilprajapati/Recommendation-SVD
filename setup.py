ee6e71ea233add055106be52a4565ba4ef587e14
pankhilprajapati <pankhil.prajapati2017@vitstudent.ac.in>
2020-04-30 15:21:57 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Recommender',
+      version='1.0',
+      description='Recommender use for user and item recomendation',
+      packages=['Recommender'],
+      author='Pankhil prajapati',
+      author_email="puruprajapti8@gmail.com",
+      zip_safe=False)
